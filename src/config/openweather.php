99b0da57f547d4742cb4985c49efa11d63440b91
ricvale <ricvale@users.noreply.github.com>
2020-06-28 03:26:24 +0100

    Update api_endpoint_onecall
    added new value 'api_endpoint_onecall'

--- src/config/openweather.php
+++ src/config/openweather.php
@@ -4,6 +4,7 @@ return [
 	'api_key' 				=> 'your-api-key',
 	'api_endpoint_current'  => 'http://api.openweathermap.org/data/2.5/weather?',
 	'api_endpoint_forecast' => 'http://api.openweathermap.org/data/2.5/forecast?',
+	'api_endpoint_onecall'  =>  'https://api.openweathermap.org/data/2.5/onecall?',
 	'api_lang' 				=> 'en',
 ];
 
