9a1efe55a0db9f0f1fb7e1cea2c72d0ff28e188a
ricvale <ricvale@users.noreply.github.com>
2020-06-28 06:11:53 +0100

    Update with new methods around getOneCallWeatherByCoords
    Methods *ByCoords were not working so:
    - Added NEW methods: getOneCallWeatherByCoords, etc.
    - Fixed bugs on coords search.

--- src/OpenWeather.php
+++ src/OpenWeather.php
@@ -11,7 +11,7 @@ use Config;
 * @package  OpenWeather
 * @author 	David Simmons <hello@dsimmons.me>
 * @license 	https://opensource.org/licenses/LGPL-3.0 LGPL-3.0
-* @version 	1.0.0
+* @version 	1.0.1 Updated & fixed by R. Vale <valenew@gmail.com> on 2020-06-28
 * @since    2019-01-01
 */
 class OpenWeather{
@@ -19,6 +19,7 @@ class OpenWeather{
 	private $api_key               = NULL;
 	private $api_endpoint_current  = NULL;
 	private $api_endpoint_forecast = NULL;
+	private $api_endpoint_onecall  = NULL;
 	private $api_lang 	           = NULL;
 
 	/**
@@ -30,6 +31,7 @@ class OpenWeather{
 		$this->api_key 				 = Config::get('openweather.api_key');
 		$this->api_endpoint_current  = Config::get('openweather.api_endpoint_current');
 		$this->api_endpoint_forecast = Config::get('openweather.api_endpoint_forecast');
+		$this->api_endpoint_onecall  = Config::get('openweather.api_endpoint_onecall');
 		$this->api_lang 			 = Config::get('openweather.api_lang');
 	}
 
@@ -212,7 +214,7 @@ class OpenWeather{
 	public function getCurrentWeatherByCoords(string $latitude, string $longitude, string $units='imperial'){
 		return $this->getCurrentWeather([
 			'lat'     => $latitude,
-			'lng'     => $longitude,
+			'lon'     => $longitude,
 			'units'   => $units,
 			'lang'    => $this->api_lang,
 			'appid'   => $this->api_key
@@ -282,7 +284,7 @@ class OpenWeather{
 	public function getForecastWeatherByCoords(string $latitude, string $longitude, string $units='imperial'){
 		return $this->getForecastWeather([
 			'lat'     => $latitude,
-			'lng'     => $longitude,
+			'lon'     => $longitude,
 			'units'   => $units,
 			'lang'    => $this->api_lang,
 			'appid'   => $this->api_key
@@ -301,6 +303,92 @@ class OpenWeather{
 		return $this->getForecastWeather([
 			'zip'     => $postal,
 			'units'   => $units,
+			'lang'    => $this->api_lang,
+			'appid'   => $this->api_key
+		]);
+	}
+	
+	
+	///////////////////////////////////////////////////////////////////////////////////////
+	
+	/**
+	 * Parses and returns an OpenWeather forecast weather API response as an array of formatted values.
+	 * Returns FALSE on failure.
+	 * 
+	 * @param  string $response OpenWeather API response JSON.
+	 * @return array
+	 */
+	 function parseOneCallResponse(string $response){
+		$struct = json_decode($response, TRUE);
+
+	 	if(!isset($struct['lat']) || !isset($struct['lon'])){
+			return FALSE;
+		}
+		return [
+		 	'timestamp' => $struct['current']['dt'],
+		 	'timezone' => $struct['timezone'],
+			'location'  => [
+				'latitude'  => $struct['lat'],
+				'longitude' => $struct['lon'],
+			],
+			'condition' => [
+				'name'     	=> $struct['current']['weather'][0]['main'],
+				'desc'     	=> $struct['current']['weather'][0]['description'],
+				'icon' 	 	=> 'http://openweathermap.org/img/w/'.$struct['current']['weather'][0]['icon'].'.png',
+			],
+			'forecast'  => [
+				'daily'  => $struct['daily']
+				
+			]
+		];
+	}
+	
+	/**
+	 * Returns an OpenWeather API response with one call.
+	 * Returns FALSE on failure.
+	 * 
+	 * @param  array $params Array of request parameters.
+	 * @return array
+	 */
+	private function getOneCallWeather(array $params){
+		$params   = http_build_query($params);
+		$request  = $this->api_endpoint_onecall.$params;
+
+		$response = $this->doRequest($request);
+		if(!$response){
+			return FALSE;
+		}
+		$response = $this->parseOneCallResponse($response);
+		if(!$response){
+			return FALSE;
+		}
+		return $response;
+	}
+	
+	/**
+	 * Returns one call weather by latitude and longitude.
+	 * Returns FALSE on failure.
+	 * 
+	 * @param  string $latitude  Latitude
+	 * @param  string $longitude Longitude
+	 * @param  string $units 	 Units of measurement (imperial, metric, kelvin)
+	 * @param  string exclude	 part (optional parameter) by using this parameter you can exclude some parts of weather data from the API response. 
+	 *				 The value parts should be a comma-delimited list (without spaces). 
+	 *				 Available values:
+	 *					current
+	 *					minutely
+	 *					hourly
+	 *					daily
+	 *				 Please note that the minute forecast is only available for certain regions.
+	 * @return array
+	 */
+	public function getOneCallWeatherByCoords(string $latitude, string $longitude, string $units='imperial', string $exclude='hourly,minutely'){
+		
+		return $this->getOneCallWeather([
+			'lat'     => $latitude,
+			'lon'     => $longitude,
+			'units'   => $units,
+			'exclude' => $exclude,
 			'lang'    => $this->api_lang,
 			'appid'   => $this->api_key
 		]);
